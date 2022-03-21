b131fc905487adfca9e127826eb8fbbaeee3e445
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-03-21 23:27:12 +0600

    second-commt

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
     return (
         <nav className='header'>
             <img src={logo} alt="" />
+            <div>
+                <a href="/shop">shop</a>
+                <a href="/orders">orders</a>
+                <a href="/inventory">inventory</a>
+                <a href="/about">about</a>
+            </div>
         </nav>
     );
 };
