10a7a33a57a21ad55d3b6cf0c87d358628440c03
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-03-22 13:56:54 +0600

    4th-commit

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './product.css'
 
 const Product = (props) => {
-    const {id, name, seller, img, category, shipping, stock,ratingsCount} = props.Product;
+    const {id, name, seller, img, category, shipping, stock,ratingsCount,price} = props.Product;
     return (
-        <div className='Product'>
+        <div className='product'>
             <img src={img} alt="" />
+            <div className='product-data'>
+            <h3>{name}</h3>
+            <p>{seller}</p>
+            <p>{price}</p>
+            </div>
         </div>
     );
 };
